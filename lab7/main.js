899433b48ff52303f14d3bffc1012d443e94f8e6
Lucas Martins <100973838+lucasrmartins0@users.noreply.github.com>
2024-11-03 21:16:24 +0000

    bueda shit feita, faltam 3 eventos para acabar o lab7

--- lab7/main.js
+++ lab7/main.js
@@ -1,7 +1,4 @@
-function mudarCor(cor) {
-    document.querySelector('#texto').style.color = cor;
-}
-
+//1
 const passarP = document.querySelector('.passar');
 const textoOriginal = passarP.textContent;
 
@@ -17,7 +14,42 @@ passarP.addEventListener('mouseover', mudaTexto);
 passarP.addEventListener('mouseout', voltaTexto);
 
 
+//2
 
+function mudarCor(cor) {
+    document.querySelector('#texto').style.color = cor;
+}
+
+
+//3
+
+const campo_texto = document.querySelector('input#escrever');
+let random = 0;
+
+function muda_aleatorio () {
+    random++;
+    if (random % 4 === 0) campo_texto.style.backgroundColor = 'purple';
+    if (random % 4 === 1) campo_texto.style.backgroundColor = 'lightblue';
+    if (random % 4 === 2) campo_texto.style.backgroundColor = 'red';
+    if (random % 4 === 3) campo_texto.style.backgroundColor = 'yellow';
+}
+
+campo_texto.addEventListener('keyup', muda_aleatorio);
+
+
+//4
+
+const inputCor = document.getElementById('cor');
+const submitButton = document.getElementById('submitbtn');
+
+submitButton.addEventListener('click', function() {
+    const color = inputCor.value.trim();
+    document.body.style.backgroundColor = color;
+});
+
+
+
+//5
 
 let counter = 0;
 const botaomais = document.getElementById('incrementar');
@@ -35,3 +67,4 @@ function decrementarBtn(){
 
 botaomais.addEventListener('click', incrementarBtn);
 botaomenos.addEventListener('click', decrementarBtn)
+
