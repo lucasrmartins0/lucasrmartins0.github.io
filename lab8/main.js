a74e3acf6b2d831351f30684effbb8a8de416d8a
Lucas Martins <100973838+lucasrmartins0@users.noreply.github.com>
2024-11-08 17:49:37 +0000

    lab8 done

--- lab8/main.js
+++ lab8/main.js
@@ -15,11 +15,12 @@ passarP.addEventListener('mouseout', voltaTexto);
 
 
 //2
-
-function mudarCor(cor) {
-    document.querySelector('#texto').style.color = cor;
-}
-
+document.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', () => {
+        const color = button.dataset.color;
+        document.getElementById('texto').style.color = color;
+    } )
+} )
 
 //3
 
@@ -38,58 +39,93 @@ campo_texto.addEventListener('keyup', muda_aleatorio);
 
 
 //4
+//----------INICIAL----------
+// const inputCor = document.getElementById('cor');
+// const submitButton = document.getElementById('submitbtn');
 
-const inputCor = document.getElementById('cor');
-const submitButton = document.getElementById('submitbtn');
-
-submitButton.addEventListener('click', function() {
-    const color = inputCor.value.trim();
-    document.body.style.backgroundColor = color;
-});
-
+// submitButton.addEventListener('click', function() {
+//     const color = inputCor.value.trim();
+//     document.body.style.backgroundColor = color;
+// });
+//---------OUTRA  MANEIRA DE FAZER--------
+// const selectCor = document.getElementById('cor');
+// selectCor.addEventListener('change', () => {
+//     const selectedCor = selectCor.value;
+//     document.body.style.backgroundColor = selectedCor
+// })
+document.querySelector('select').onchange = function() {
+    // Altera o background color do corpo da página para o valor do select
+    document.querySelector('body').style.backgroundColor = this.value;
+};
 
 //5
 
 let counter = 0;
 const botaomais = document.getElementById('incrementar');
-const botaomenos = document.getElementById('decrementar')
+const botaomenos = document.getElementById('decrementar');
 const elementoContador = document.getElementById('contador');
 
-function incrementarBtn() {
+const incrementarBtn = () => {
     counter++;
-    elementoContador.textContent = counter; 
-}
-function decrementarBtn(){
+    elementoContador.textContent = counter;
+};
+
+const decrementarBtn = () => {
     counter--;
     elementoContador.textContent = counter;
-}
+};
 
 botaomais.addEventListener('click', incrementarBtn);
-botaomenos.addEventListener('click', decrementarBtn)
-
+botaomenos.addEventListener('click', decrementarBtn);
 
 //6
-function showMensagem() {
-    const nome = document.getElementById("nomee").value;
-    const idade = document.getElementById("idadee").value;
+// function showMensagem() {
+//     const nome = document.getElementById("nomee").value;
+//     const idade = document.getElementById("idadee").value;
     
-    const mensagem = `Yo, ${nome} tens ${idade} anus!`;
+//     const mensagem = `Yo, ${nome} tens ${idade} anus!`;
     
-    document.getElementById("mensagem").textContent = mensagem;
-}
+//     document.getElementById("mensagem").textContent = mensagem;
+// }
 
-document.getElementById("BotaoSubmeter").addEventListener("click", showMensagem);
+// document.getElementById("BotaoSubmeter").addEventListener("click", showMensagem);
+
+document.querySelector('#formulario').onsubmit = (e) => {
+    // Não deixa F5
+    e.preventDefault();
+
+    const nome = document.querySelector('#nomee').value;
+    const idade = document.querySelector('#idadee').value;
+
+    document.querySelector('#mensagem').textContent = `Yo, ${nome} tens ${idade} anus!`;
+};
 
 
 //7
+// const contadorDisplay = document.getElementById("contador7");
+
+// let counter2 = 0;
+
+// function incrementarAutomatico() {
+//     counter2++; 
+//     contadorDisplay.textContent = counter2;
+// }
+
+// setInterval(incrementarAutomatico, 1000);
 const contadorDisplay = document.getElementById("contador7");
 
-let counter2 = 0;
+    let counter2 = localStorage.getItem('counter') ? parseInt(localStorage.getItem('counter')) : 0;
 
-function incrementarAutomatico() {
-    counter2++; 
     contadorDisplay.textContent = counter2;
-}
 
-// Define um intervalo de 1 segundo (1000 ms) para chamar a função
-setInterval(incrementarAutomatico, 1000);
+    function incrementarAutomatico() {
+        counter2++; 
+        contadorDisplay.textContent = counter2;
+
+        localStorage.setItem('counter', counter2);
+    }
+
+    setInterval(incrementarAutomatico, 1000);
+
+
+
